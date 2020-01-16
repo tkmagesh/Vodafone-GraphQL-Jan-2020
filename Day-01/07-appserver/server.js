1152c0a31f81298ff8f73a3ba6cdbebdc67934e4
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-16 10:32:41 +0530

    Handling post requests

--- Day-01/07-appserver/server.js
+++ Day-01/07-appserver/server.js
@@ -13,14 +13,30 @@ const server = http.createServer((req, res) => {
         res.end();
         return;
     }
-    const op = query.op,
-        x = parseInt(query.x),
-        y = parseInt(query.y);
-    console.log(op, x, y);
-    const result = calculator[op](x,y);
+    if (req.method === 'GET'){
+        const op = query.op,
+            x = parseInt(query.x),
+            y = parseInt(query.y);
+        console.log(op, x, y);
+        const result = calculator[op](x,y);
 
-    res.write(result.toString());
-    res.end();
+        res.write(result.toString());
+        res.end();
+    } else {
+        let rawData = '';
+        req.on('data', chunk => rawData += chunk);
+        req.on('end', () => {
+            console.log(rawData);
+            const body = querystring.parse(rawData);
+            const op = body.op,
+                x = parseInt(body.x),
+                y = parseInt(body.y);
+            console.log(op, x, y);
+            const result = calculator[op](x, y);
+            res.write(result.toString());
+            res.end();
+        })
+    }
 })
 server.listen(8085);
 server.on('listening', () => console.log('App Server listening on 8085'));
