ac0b9dbf2361b71a7c7bace97a7d2f12e45ebac6
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-17 17:03:41 +0530

    Apollo Subscriptions

--- Day-03/14-graphql-server/server.js
+++ Day-03/14-graphql-server/server.js
@@ -1,10 +1,15 @@
 const express = require('express'),
     apolloServerExpress = require('apollo-server-express'),
     axios = require('axios'),
-    gql = apolloServerExpress.gql;
+    gql = apolloServerExpress.gql,
+    http = require('http');
+
+const PubSub = require('apollo-server').PubSub
 
 
 const app = express();
+
+const pubsub = new PubSub();
 
 const schema = gql`
     
@@ -46,7 +51,21 @@ const schema = gql`
 
     type Mutation{
         addProduct(id : ID!, name : String, cost : Int, units : Int, inStock : Boolean) : Product,
-        addCategory(id : ID!, name : String, products : [ProductData]) : Category
+        addCategory(id : ID!, name : String, products : [ProductData]) : Category,
+        createMessage : Message
+    }
+    
+
+    type Message {
+        id: ID!
+        text: String!
+        createdAt: String!
+    }
+    type Subscription {
+        messageCreated: MessageCreated!
+    }
+    type MessageCreated {
+        message: Message!
     }
 `
 
@@ -93,16 +112,58 @@ const resolvers = {
             const newCategory = { id, name, products };
             categoryList.push(newCategory);
             return newCategory;
+        },
+        createMessage : () => {
+            pubsub.publish("CREATED", {
+                messageCreated: { 
+                    message : { id : "1", text : "Something is created", createdAt : new Date().toString() }
+                },
+            });
         }
-    }
+    }, 
+    Subscription: {
+        messageCreated: {
+            subscribe: () => pubsub.asyncIterator("CREATED"),
+        },
+    },
 }
 const apolloServer = new apolloServerExpress.ApolloServer({
     typeDefs: schema,
     resolvers,
 });
 
+
 apolloServer.applyMiddleware({ app, path : '/graphql'})
 
-app.listen({ port: 8085 }, () => {
+const httpServer = http.createServer(app); 
+apolloServer.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({ port: 8085 }, () => {
     console.log('Apollo Server on http://localhost:8085/graphql');
 });
+
+/* 
+//For subscription
+
+subscription {
+  messageCreated {
+message {
+  id
+      text
+      createdAt
+
+} }
+}
+
+
+//For triggering the message
+mutation{
+  createMessage{
+    id,
+    text,
+    createdAt
+  }
+}
+
+
+*/
